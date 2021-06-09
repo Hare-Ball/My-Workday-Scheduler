7029908b1815729d42acbc9e7904b2466691b7ad
Hare-Ball <robby.hare@gmail.com>
2021-06-08 22:50:00 -0500

    updates to JavaScript

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,28 @@ $("#currentDay").html(today);
 var clock = moment().format('h:mm a');
 $("#digitalClock").html(clock);
 
+
+//Create save function to add data to local storage
+// get the text
+var text = $(this).siblings(".description").text();
+
+var time = $(this).parent().attr("id");
+
+
+// set the item in localStorage
+localStorage.setItem(text, time);
+
+$("#9 .description").val(localStorage.getItem("9"));
+$("#8 .description").val(localStorage.getItem("8"));
+$("#10 .description").val(localStorage.getItem("10"));
+$("#11 .description").val(localStorage.getItem("11"));
+$("#12 .description").val(localStorage.getItem("12"));
+$("#13 .description").val(localStorage.getItem("13"));
+$("#14 .description").val(localStorage.getItem("14"));
+$("#15 .description").val(localStorage.getItem("15"));
+$("#16 .description").val(localStorage.getItem("16"));
+$("#17 .description").val(localStorage.getItem("17"));
+
 function timeReader() {
         //get current number of hours.
         var currentTime = moment().hour();
@@ -17,6 +39,7 @@ function timeReader() {
             // To check the time and add css classes
             if (timeSlot < currentTime) {
                 $(this).addClass("past");
+                
             }
             else if (timeSlot === currentTime) {
                 $(this).addClass("present");
@@ -29,5 +52,3 @@ function timeReader() {
 }
 
 timeReader();
-
-//Create save function to add data to local storage
