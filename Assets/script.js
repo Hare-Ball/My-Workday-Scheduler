89e8b84b8fe204f63c7df7c1170eb3d8cabcb625
Hare-Ball <robby.hare@gmail.com>
2021-06-08 23:22:49 -0500

    restructure the documents

--- Assets/script.js
+++ Assets/script.js
@@ -7,26 +7,17 @@ var clock = moment().format('h:mm a');
 $("#digitalClock").html(clock);
 
 
-//Create save function to add data to local storage
-// get the text
-var text = $(this).siblings(".description").text();
+//Add data to local storage
 
-var time = $(this).parent().attr("id");
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        // Trying to use local storage in JQuery
+        var text = $(this).siblings(".description").text();
+        var savedText = $(this).siblings(".description").text();
+      
+        localStorage.setItem(text, savedText);
+    })
 
-
-// set the item in localStorage
-localStorage.setItem(text, time);
-
-$("#9 .description").val(localStorage.getItem("9"));
-$("#8 .description").val(localStorage.getItem("8"));
-$("#10 .description").val(localStorage.getItem("10"));
-$("#11 .description").val(localStorage.getItem("11"));
-$("#12 .description").val(localStorage.getItem("12"));
-$("#13 .description").val(localStorage.getItem("13"));
-$("#14 .description").val(localStorage.getItem("14"));
-$("#15 .description").val(localStorage.getItem("15"));
-$("#16 .description").val(localStorage.getItem("16"));
-$("#17 .description").val(localStorage.getItem("17"));
 
 function timeReader() {
         //get current number of hours.
@@ -52,3 +43,4 @@ function timeReader() {
 }
 
 timeReader();
+})
